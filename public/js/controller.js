1ee77d070cd6e5ea2552774439696057a8a99363
soxsnation <soxsnation@gmail.com>
2014-09-10 17:58:44 -0400

    updated home page, added mortgage calculator

--- public/js/controller.js
+++ public/js/controller.js
@@ -8,7 +8,17 @@
 /* Controllers */
 
 function HomeController($scope, $http) {
+	$scope.monthlyPayment = 0;
+	$scope.rate = 3.95;
+	$scope.term = 30;
+	$scope.price = 450000;
+	$scope.downPayment = 0;
 
+	$scope.calculatePayments = function() {
+		var t = (($scope.rate / 100 / 12) * ($scope.price - $scope.downPayment));
+		var b = (1 - Math.pow(1 + ($scope.rate / 100 / 12), $scope.term * -12));		
+		$scope.monthlyPayment =  Math.round(t / b);
+	}
 
 	$scope.currentPage = 'Home';
 	$scope.intro = 'We\'re glad you\'re here! We have been serving West Volusia and Seminole counties since 1990 providing over 30 years of real estate experience. We pride ourselves on repeat business and referrals from satisfies clients. Come get to know us at Discom Realty and <i>discover</i> our <i>committment</i> to you, our newest neighbor';
